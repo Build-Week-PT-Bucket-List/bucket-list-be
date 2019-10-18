234acacaef389f36e3c22becdb81e2fd50f9d9d8
cecil john tantay <cjbtantay@gmail.com>
2019-10-17 21:52:46 -0700

    chnages

--- api/models/contactsModel.js
+++ api/models/contactsModel.js
@@ -1,37 +1,36 @@
-const db = require("../../data/dbConfig");
+const db = require('../../data/dbConfig');
 
 module.exports = {
-    getContacts,
-    addContact,
-    getMessage,
-    addMessage,
-    deleteContact,
-}
+  getContacts,
+  addContact,
+  getMessage,
+  addMessage,
+  deleteContact
+};
 
 function getContacts() {
-    return db('contacts');
+  return db('contacts');
 }
 
-async function addContact(contact){
-    const [newContact] = await db('contacts').insert(contact, ["id"]);
-    return newContact.id;
+async function addContact(contact) {
+  const [newContact] = await db('contacts').insert(contact, ['id']);
+  return newContact;
 }
 
-function deleteContact(id){
-    return db('contacts')
+function deleteContact(id) {
+  return db('contacts')
     .where({ id })
     .del();
 }
 
-function getMessage(contact_id){
-    return db
+function getMessage(contact_id) {
+  return db
     .select('message')
     .from('messages')
-    .where({contact_id});
-     
+    .where({ contact_id });
 }
 
-async function addMessage(message){
-    const [newMessage] = await db('messages').insert(message, ["id"]);
-    return newMessage.id;
+async function addMessage(message) {
+  const [newMessage] = await db('messages').insert(message, ['id']);
+  return newMessage.id;
 }
